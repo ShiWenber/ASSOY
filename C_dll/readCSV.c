fbf1c7a73609d1739dfbe5a8082b8eab612a6039
wenboshicn <ynu20201120443@mail.ynu.edu.cn>
2022-02-24 21:08:08 +0800

    将当前目录创建为python虚拟环境

---
+++ C_dll/readCSV.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+# include<stdlib.h>
+
+int readCSV(char * inputCSVfile){
+    FILE *fp;
+    FILE *fp1;
+    fp = fopen(inputCSVfile,"r");
+    fp1 = fopen(inputCSVfile,"r")
+    if(fp == NULL){
+        printf("${1|,content,hello world,done,begin,end|}");
+    }
+}
+
+int main(){
+    
+    return 0;
+}
