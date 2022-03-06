39bff1c28d4d263bb2b75ef06da6caa5aa327228
wenboshicn <ynu20201120443@mail.ynu.edu.cn>
2022-03-06 16:01:18 +0800

    删除venv虚拟环境并去除多余文件，添加了项目存储表格的文件

---
+++ src/exercise/test.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+class Weekday():
+    monday = 1
+    tuestday = 2
+    wednesday = 3
+    thirsday = 4
+    friday = 5
+    saturday = 6
+    sunday = 7
+class status():
+    noClass = 0 # no class
+    oddWeek = 1 # 单周有课
+    evenWeek = 2 # 双周有课
+    
+    
+# test
+    
+
+
+# index 是列索引
+# columns 是行索引    
+def extract_schedule(sheetpath):
+    """
+    Extracts the schedule from the sheet and returns a list of dictionaries
+    """    
+
+
+# # 需要遍历文件
+# def     
+    
+if __name__ == "__main__":
+    extract_schedule('E:\\github\\Automatic_Scheduling_for_Student_Organization_in_YNU_C\\ASSOY\\inputExamples\\timeTableForStu1.xlsx')
