fa7b2a4a2e64312a3680a7ff1872b955b153ca61
ShiWenber <1210169842@qq.com>
2022-06-29 19:27:58 +0800

    feat(src): 第一版归档

--- src/import_file.py
+++
@@ -1,9 +0,0 @@
-# 创建一个只有按钮的页面
-
-from sre_parse import SubPattern
-
-
-class MainForm(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
--- src/main.py
+++ src/main.py
@@ -18,3 +18,28 @@ def main():
 if __name__ == '__main__':
     print('开始运行')
     main()
+
+    
+    # # -----------调用自动做系统图的工具
+    # from pycallgraph import PyCallGraph
+    # from pycallgraph.output import GraphvizOutput
+    # from pycallgraph import Config
+    # from pycallgraph import GlobbingFilter
+
+  
+    # ----------使用自动调用系统图工具
+    # config = Config()
+    # # 添加需要绘制的函数名或类名
+    # config.trace_filter = GlobbingFilter(include=[
+    #     'main',
+    #     # 'ui.*',
+    #     'functions.*',
+    #     'page_controller.*',
+    # ])
+    # # 以下添加需要剔除的函数名或类名
+    # # config.trace_filter = GlobbingFilter(exclude=[])
+
+    # graphviz = GraphvizOutput()
+    # graphviz.output_file = './graph/calling_graph'+'.png'   # 要保存的路径名和文件名
+    # with PyCallGraph(output=graphviz, config=config):
+    #     main()
